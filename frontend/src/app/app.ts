2e241bc290b800b93965d5f9a6cba2751eb9b86d
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-08 16:37:35 -0500

    Actualizando tutorial Angular 20

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.html',
-    styleUrl: './app.css'
+  selector: 'app-root',
+  imports: [RouterOutlet, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-    protected title:number = 35;
+  protected title:number = 35;
 }
